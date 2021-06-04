f4b67768ccf9692c44be7907e6d138508cfb0b30
venk2622 <venkisaravan007@gmail.com>
2021-06-04 19:16:11 +0530

    feature 1 updated

--- sql/categoriesTable.sql
+++ sql/categoriesTable.sql
@@ -7,21 +7,20 @@ create table shows (
 	language varchar (50) not null,
 	category varchar(50) not null,
 	membership varchar(20) not null,
-	check (membership in ('prime','non prime'))
+	check (membership in ('prime','non prime')),
 	grade varchar(2) not null,
 	check (grade in ('U','V','A'))
 );
-INSERT INTO categories (genre,name,year,language,category,membership,grade)
+INSERT INTO shows (genre,name,year,language,category,membership,grade)
 values ('drama','bigil','2019','tamil','movie','non prime','U');
-INSERT INTO categories (id,genre,name,year,language,category,membership,grade)
+INSERT INTO shows (genre,name,year,language,category,membership,grade)
 values ('drama','sivaji','2012','tamil','movie','prime','U');
-INSERT INTO categories (id,genre,name,year,language,category,membership,grade)
+INSERT INTO shows (genre,name,year,language,category,membership,grade)
 values ('kids','dora','2020','english','movie','prime','V');
-INSERT INTO categories (id,genre,name,year,language,category,membership,grade)
+INSERT INTO shows (genre,name,year,language,category,membership,grade)
 values ('action','explosion','2018','english','movie','prime','U');
-INSERT INTO categories (id,genre,name,year,language,category,membership,grade)
+INSERT INTO shows (genre,name,year,language,category,membership,grade)
 values ('action','fast and furious','2009','english','movie','prime','U');
-select * from categories;
 
 
 select *from categories
