82f7d74a99b4b319b27f9711e623715da1fab8b6
venk2622 <venkisaravan007@gmail.com>
2021-06-04 14:06:25 +0530

    feature 1 logger fix

--- src/main/java/in/venkat/util/Logger.java
+++ src/main/java/in/venkat/util/Logger.java
@@ -1,6 +1,12 @@
 package in.venkat.util;
 
 public class Logger {
+	private Logger() {
+		/**
+		 * Adding private Constructor
+		 */
+	}
+
 	public static void log(Object obj) {
 		System.out.println(obj);
 	}
