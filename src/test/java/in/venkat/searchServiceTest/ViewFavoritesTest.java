71b03e29d524d326715d5127a1c5d567b33b4708
venk2622 <venkisaravan007@gmail.com>
2021-06-20 23:09:41 +0530

    feature 9 bugfix

---
+++ src/test/java/in/venkat/searchServiceTest/ViewFavoritesTest.java
@@ -0,0 +1,32 @@
+package in.venkat.searchServiceTest;
+
+import java.util.List;
+
+import in.venkat.exceptions.DbException;
+import in.venkat.exceptions.InvalidUserIdException;
+import in.venkat.model.Show;
+import in.venkat.service.ShowService;
+import in.venkat.util.Logger;
+
+public class ViewFavoritesTest {
+
+	public static void main(String[] args) {
+		String userId = "venkat9790430272";
+
+		viewFavoriteMovies(userId);
+
+	}
+
+	private static void viewFavoriteMovies(String userId) {
+
+		try {
+			List<Show> favorites = ShowService.viewFavoriteMovies(userId);
+			Logger.log(favorites);
+
+		} catch (DbException | InvalidUserIdException e) {
+			Logger.exception(e);
+		}
+
+	}
+
+}
